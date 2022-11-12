6a31d5e0f74d0e0b71bbcf5efa0189ca8a2ed69a
Minura Hajisoy <minura.hajisoy@gmail.com>
2022-11-12 09:31:38 +0400

    Adding code

---
+++ breastcancer/breastcancer/Form2.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace breastcancer
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        int c = 0;
+        string[] files;
+        String pathPC = "E:\\OneDrive - ADA University\\Homework\\Fall2022\\SDP\\PotentialCancer\\";
+        String pathC = "E:\\OneDrive - ADA University\\Homework\\Fall2022\\SDP\\Cancer\\";
+        String pathNC = "E:\\OneDrive - ADA University\\Homework\\Fall2022\\SDP\\NotCancer\\";
+
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+
+
+            pictureBox1.Invoke((MethodInvoker)(() => pictureBox1.Location = new Point(30, -10)));
+            //pictureBox2.Invoke((MethodInvoker)(() => pictureBox2.Location = new Point(, )));
+
+            label1.Invoke((MethodInvoker)(() => label1.Location = new Point(526, 650)));
+
+            this.BackColor = Color.FromArgb(255, 254, 252);
+            //this.pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+
+            Size size = new Size(600, 700);
+            pictureBox1.Size = size;
+
+            this.WindowState = FormWindowState.Maximized;
+            label1.BackColor = Color.Transparent;
+            label1.Visible = false;
+            radioButton1.Visible = false;
+            radioButton2.Visible = false;
+            radioButton3.Visible = false;
+
+            string filePath = @"E:\\OneDrive - ADA University\\Homework\\Fall2022\\SDP\\Images";
+            files = Directory.GetFiles(filePath);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+
+            if (c == files.Length - 1)
+                button2.Enabled = false;
+            else
+            {
+                c++;
+                button1.Enabled = true;
+                label1.Text = c + 1 + " out of " + files.Length + " images \n";// + files[c];
+
+                pictureBox1.Image = Image.FromFile(files[c]);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(files[c]);
+            label1.Visible = true;
+            radioButton1.Visible = true;
+            radioButton2.Visible = true;
+            radioButton3.Visible = true;
+            label1.Text = c + 1 + " out of " + files.Length + " images \n";// + files[c];
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            radioButton3.Checked = false;
+
+            if (c == 0)
+                button1.Enabled = false;
+            else
+            {
+                c--;
+                button2.Enabled = true;
+                label1.Text = c + 1 + " out of " + files.Length + " images \n" + files[c];
+
+                pictureBox1.Image = Image.FromFile(files[c]);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            string fullPath = Path.GetFullPath(files[c]).TrimEnd(Path.DirectorySeparatorChar);
+            string projectName = Path.GetFileName(fullPath);
+           // MessageBox.Show(projectName);
+            String pathCan = pathC + projectName;
+            //MessageBox.Show(pathCan);
+            pictureBox1.Image.Save(pathCan);//, ImageFormat.Jpeg);
+
+            if (File.Exists(pathPC + projectName))
+            {
+                File.Delete(pathPC + projectName);
+            }
+            if (File.Exists(pathNC + projectName))
+            {
+                File.Delete(pathNC + projectName);
+            }
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            string fullPath = Path.GetFullPath(files[c]).TrimEnd(Path.DirectorySeparatorChar);
+            string projectName = Path.GetFileName(fullPath);
+            //MessageBox.Show(projectName);
+            //pictureBox1.Image.Save("myfile.png", ImageFormat.Png);
+            String pathCan = pathPC + projectName;
+            //MessageBox.Show(pathCan);
+            pictureBox1.Image.Save(pathCan);//, ImageFormat.Jpeg);
+            if (File.Exists(pathC + projectName))
+            {
+                File.Delete(pathC + projectName);
+            }
+            if (File.Exists(pathNC + projectName))
+            {
+                File.Delete(pathNC + projectName);
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            string fullPath = Path.GetFullPath(files[c]).TrimEnd(Path.DirectorySeparatorChar);
+            string projectName = Path.GetFileName(fullPath);
+            //MessageBox.Show(projectName);
+            //pictureBox1.Image.Save("myfile.png", ImageFormat.Png);
+            String pathCan = pathNC + projectName;
+            //MessageBox.Show(pathCan);
+            pictureBox1.Image.Save(pathCan);//, ImageFormat.Jpeg);
+
+            if (File.Exists(pathC + projectName))
+            {
+                File.Delete(pathC + projectName);
+            }
+            if (File.Exists(pathPC + projectName))
+            {
+                File.Delete(pathPC + projectName);
+            }
+        }
+    }
+}
---
+++ breastcancer/breastcancer/Form3.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace breastcancer
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(38, 403);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(270, 199);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.HotPink;
+            this.label1.Location = new System.Drawing.Point(522, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(233, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Early detection saves life!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.HotPink;
+            this.label2.Location = new System.Drawing.Point(12, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "How do you know?";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.HotPink;
+            this.label3.Location = new System.Drawing.Point(439, 384);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "About the application";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.HotPink;
+            this.label4.Location = new System.Drawing.Point(578, 133);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(649, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "How to reduce your chance of developing breast cancer, or surviving it?";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(238, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(800, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "“When detected early in the localized stage, before it has a chance to spread, th" +
+    "e 5-year survival rate is 99%,”";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(12, 158);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(423, 147);
+            this.label6.TabIndex = 10;
+            this.label6.Text = resources.GetString("label6.Text");
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(424, 409);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(846, 125);
+            this.label7.TabIndex = 11;
+            this.label7.Text = resources.GetString("label7.Text");
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(578, 158);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(678, 168);
+            this.label8.TabIndex = 12;
+            this.label8.Text = resources.GetString("label8.Text");
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1299, 614);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+    }
+}

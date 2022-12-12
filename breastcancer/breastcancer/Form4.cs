2e3f422da6aa5b42abf7dd23538aaaaa2d117944
Minura Hajisoy <minura.hajisoy@gmail.com>
2022-12-12 18:31:54 +0400

    form4 update1

--- breastcancer/breastcancer/Form4.cs
+++ breastcancer/breastcancer/Form4.cs
@@ -14,7 +14,6 @@ namespace breastcancer
     {
         int picNum4;
         bool downFlag4;
-
         public Form4()
         {
             InitializeComponent();
@@ -28,40 +27,11 @@ namespace breastcancer
         private Font fnt = new Font("Arial", 10);
         string filepath;
 
-        //private void Form1_Load(object sender, EventArgs e)
-        //{
-        //    // Dock the PictureBox to the form and set its background to white.
-        //    pictureBox1.Image = Image.FromFile(@"E:\OneDrive - ADA University\Homework\Fall2022\SDP\Images\image0.png"); // Use a valid image path
-
-        //    // pictureBox1.Dock = DockStyle.Fill;
-        //    // pictureBox1.BackColor = Color.White;
-        //    // Connect the Paint event of the PictureBox to the event handler method.
-        //    //    pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
-
-        //    // Add the PictureBox control to the Form.
-        //    //    this.Controls.Add(pictureBox1);
-        //}
-
-        //private void pictureBox1_Paint(object sender, PaintEventArgs e)
-        //{
-        //    // Create a local version of the graphics object for the PictureBox.
-        //    //Graphics g = e.Graphics;
-
-        //    //// Draw a string on the PictureBox.
-        //    //g.DrawString("This is a diagonal line drawn on the control",
-        //    //    fnt, System.Drawing.Brushes.Blue, new Point(30, 30));
-        //    //// Draw a line in the PictureBox.
-        //    //g.DrawLine(System.Drawing.Pens.Red, pictureBox1.Left, pictureBox1.Top,
-        //    //    pictureBox1.Right, pictureBox1.Bottom);
-        //}
-
         private void Form4_Load(object sender, EventArgs e)
         {
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             this.WindowState = FormWindowState.Maximized;
             this.BackColor = Color.FromArgb(34, 34, 34);
-            //  string filePath;// = @"E:\OneDrive - ADA University\Homework\SDP2022-brstcancer\Photos\NotResized";
-
 
             string filePathBrightened = @"E:\OneDrive - ADA University\Homework\SDP2022-brstcancer\Augmentation\Brightened";
             string filePathColor1 = @"E:\OneDrive - ADA University\Homework\SDP2022-brstcancer\Augmentation\Color1";
@@ -125,23 +95,17 @@ namespace breastcancer
                 filepath = filePathResizedTo1023;
             }
 
-
             files = Directory.GetFiles(filepath);
             pictureBox1.Image = Image.FromFile(files[c]);
         }
-
         private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            //new Form2().Show();
             this.Close();
-
         }
-
         private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
         {
 
         }
-
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
         {
             if (e.Delta != 0)
@@ -163,26 +127,5 @@ namespace breastcancer
                 pictureBox1.Height += Convert.ToInt32(pictureBox1.Height * e.Delta / 1000);
             }
         }
-
-        private void buttonPrevious_Click(object sender, EventArgs e)
-        {
-
-            // pictureBox3.Image = Image.FromFile(filesNotResized[c]);
-
-        }
-
-        //private void pictureBox1_Paint(object sender, PaintEventArgs e)
-        //{
-        //    Create a local version of the graphics object for the PictureBox.
-
-        //   Graphics g = e.Graphics;
-
-        //    Draw a string on the PictureBox.
-        //    g.DrawString("This is a diagonal line drawn on the control",
-        //        fnt, Brushes.Blue, new Point(30, 30));
-        //    Draw a line in the PictureBox.
-        //    g.DrawLine(Pens.Red, pictureBox1.Left, pictureBox1.Top,
-        //        pictureBox1.Right, pictureBox1.Bottom);
-        //}
     }
 }
